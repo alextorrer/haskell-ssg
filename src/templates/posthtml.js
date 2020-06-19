1381aeeea621a2afefcd484731fa1beda792a67b
alextorrer <alextorre0427@gmail.com>
2020-06-18 20:27:23 -0500

    Add back arrow on post

--- src/templates/posthtml.js
+++ src/templates/posthtml.js
@@ -7,11 +7,12 @@ const posthtml = (data) => `
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${data.attributes.description}" />
+        <link href="../assets/main.css" rel="stylesheet" type="text/css"/>
         <title>${data.attributes.title}</title>
     </head>
     <body>
         <header>
-            <a href="/">Go back home</a>
+            <a href="/"><img class="arrow-icon" src="../assets/img/arrow-back.png" alt="arrow icon"></a>
         </header>
         <div class="content">
             <h1>${data.attributes.title}</h1>
